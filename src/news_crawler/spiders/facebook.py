36be288b2d16651d367bf8975566e468e58a2e8a
heitor57 <heitorwerneck@hotmail.com>
2021-02-28 20:13:31 -0300

    Facebook process

---
+++ src/news_crawler/spiders/facebook.py
@@ -0,0 +1,19 @@
+import selenium
+def facebook_process(driver):
+    try:
+        element = driver.find_element_by_xpath("//button[@class='_1gl3 _4jy0 _4jy3 _517h _51sy _42ft']")
+        while element:
+            element.click()
+            time.sleep(2)
+            element = driver.find_element_by_xpath("//button[@class='_1gl3 _4jy0 _4jy3 _517h _51sy _42ft']")
+    except selenium.common.exceptions.NoSuchElementException as e:
+        pass
+
+    try:
+        element = driver.find_element_by_xpath("//div[@class='_5yct _3-8y _3-96 _2ph-']//a")
+        while element:
+            element.click()
+            time.sleep(2)
+            element = driver.find_element_by_xpath("//div[@class='_5yct _3-8y _3-96 _2ph-']//a")
+    except selenium.common.exceptions.NoSuchElementException as e:
+        pass
