43d4bf56ee35c7da57197f35948a8d98e1b15643
heitor57 <heitorwerneck@hotmail.com>
2021-03-01 11:12:27 -0300

    Items definition

--- src/news_crawler/items.py
+++ src/news_crawler/items.py
@@ -10,3 +10,26 @@ class NewsCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class Publication(scrapy.Item):
+    title = scrapy.Field()
+    subtitle = scrapy.Field()
+    author = scrapy.Field()
+    last_modification = scrapy.Field()
+    text = scrapy.Field()
+    obtained = scrapy.Field()
+    main_image = scrapy.Field()
+    read_time = scrapy.Field()
+    url = scrapy.Field()
+    key_words = scrapy.Field()
+
+class Pessoa(scrapy.Item):
+    profile_image = scrapy.Field()
+    url = scrapy.Field()
+    name = scrapy.Field()
+
+class Author(Pessoa):
+    key_words = scrapy.Field()
+    description = scrapy.Field()
+    
+
